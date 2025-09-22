4c1ad34bd2740dac02bb7f01c2505ebeb582b42d
kdsousa <keplin.s.sousa@outlook.com>
2025-09-22 02:49:30 -0300

    aula 02 adicionando primeira parte dos exercicios

---
+++ exercicios.py
@@ -0,0 +1,59 @@
+# Exercícios
+# Inteiros (int)
+# 1-Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# n1 = int(input('Digite um número: '))
+# n2 = int(input('Digite outro número: '))
+# soma = n1 + n2
+# print(f'A soma dos números {n1} e {n2} é igual a {soma}')
+
+# 2-Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# n1 = int(input('Digite um número: '))
+# divisor = 5
+# divisao = n1 / divisor
+# print(f'Seu número dividido por {divisor} é {divisao}')
+
+# 3-Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# n1 = int(input('Digite um número: '))
+# n2 = int(input('Digite outro número: '))
+# multiplica = n1 * n2
+# print(f'A multiplicação entre {n1} e {n2} é igual a {multiplica}')
+
+# 4-Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# n1 = int(input('Digite o primeiro número: '))
+# n2 = int(input('Digite o segundo número: '))
+# divisao = n1 // n2
+# print(f'A divisão do {n1} pelo {n2} é {divisao}')
+
+# 5-Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# n1 = int(input('Digite um numero: '))
+# quadrado = n1 * n1
+# print(f'O seu número ao quadrado é {quadrado}')
+
+#Números de Ponto Flutuante (float)
+# 6-Escreva um programa que receba dois números flutuantes e realize sua adição.
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite outro número: '))
+# soma = n1 + n2
+# print(f'A soma de {n1} e {n2}, é {soma}')
+
+# 7-Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite outro número: '))
+# media = (n1 + n2) / 2
+# print(f'A media é de {media}')
+
+# 8-Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# base = float(input('Digite a base para a potenciação: '))
+# expoente = float((input('Digite o expoente para a potenciação: ')))
+# potencia = base ** expoente
+# print(f'Resultado {potencia}')
+
+# 9-Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# temp_celsius = float(input('Digite a temperatura em celsius: '))
+# temp_fahrenheit = (temp_celsius * 1.8) + 32.0
+# print(f'A tempereatura em fahrenheit é de {temp_fahrenheit}')
+
+# 10-Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+raio = float(input('Digite o raio do circulo: '))
+area = 3.14 * raio * raio
+print( f'A área é de: {area}')
